d85bd94d3132b3f30b66445befb598a3052bc1e6
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-31 15:23:34 -0800

    Bump i18n

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/openvoxproject/stockpile"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.openvoxproject/i18n "0.9.3"]
+  :dependencies [[org.openvoxproject/i18n "0.9.4"]
                  [org.clojure/clojure "1.8.0"]]
-  :plugins [[org.openvoxproject/i18n "0.9.3"]]
+  :plugins [[org.openvoxproject/i18n "0.9.4"]]
   :profiles {:dev {:dependencies [[org.apache.commons/commons-lang3 "3.4"]]}}
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
