46e9d6576e578dc0a366288e065c355799c4b7a8
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-01-09 01:33:41 -0800

    Update clojure and i18n versions

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
+(def i18n-version "1.0.2")
+
 (defproject org.openvoxproject/stockpile "1.0.1-SNAPSHOT"
   :description "Simple, durable Clojure queuing library"
   :url "https://github.com/openvoxproject/stockpile"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.openvoxproject/i18n "1.0.1"]
-                 [org.clojure/clojure "1.11.2"]] ;; Match what is used in clj-parent
-  :plugins [[org.openvoxproject/i18n "1.0.1"]]
+  :dependencies [[org.openvoxproject/i18n ~i18n-version]
+                 [org.clojure/clojure "1.12.4"]]
+  :plugins [[org.openvoxproject/i18n ~i18n-version]]
   :profiles {:dev {:dependencies [[org.apache.commons/commons-lang3 "3.18.0"]]}}
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
