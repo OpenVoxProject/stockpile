6ff1f03b52e40fa78c416f6f755b2f4c079311d5
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-06-24 15:38:47 +0000

    Version 1.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def i18n-version "1.0.4")
 
-(defproject org.openvoxproject/stockpile "1.0.2"
+(defproject org.openvoxproject/stockpile "1.0.3-SNAPSHOT"
   :description "Simple, durable Clojure queuing library"
   :url "https://github.com/openvoxproject/stockpile"
   :license {:name "Apache License Version 2.0"
